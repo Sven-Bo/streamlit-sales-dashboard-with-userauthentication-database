f1bc0900aae9e3f6e492f1a65206610cb4afcf8b
Sven <67136317+Sven-Bo@users.noreply.github.com>
2022-09-17 22:57:39 +0200

    Add info for test credentials

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import database as db
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
+st.info("CREDENTIALS | username:pparker ; password:abc123")
 
 # --- USER AUTHENTICATION ---
 users = db.fetch_all_users()
